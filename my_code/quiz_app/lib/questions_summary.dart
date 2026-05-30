2b61d7431ee818d850cc0511064c7f32707cf348
Rich Friendlich <rfriend@yahoo.com>
2026-05-30 12:16:34 -0400

    tweaked results scrolling

--- my_code/quiz_app/lib/questions_summary.dart
+++ my_code/quiz_app/lib/questions_summary.dart
@@ -10,57 +10,52 @@ class QuestionsSummary extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 300,
-      child: Scrollbar(
-        interactive: true,
-        thumbVisibility: true,
-        child: SingleChildScrollView(
-          child: Column(
-            children: summaryData.map((data) {
-              return Row(
-                children: [
-                  CircleAvatar(
-                    radius: 25,
-                    backgroundColor:
-                        (data['user_answer'] == data['correct_answer'])
-                        ? const Color.fromARGB(255, 7, 184, 80)
-                        : const Color.fromARGB(255, 220, 20, 60),
-                    child: Text(
-                      ((data['question_index'] as int) + 1).toString(),
-                      style: const TextStyle(color: Colors.white),
-                    ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map((data) {
+            return Row(
+              children: [
+                CircleAvatar(
+                  radius: 25,
+                  backgroundColor:
+                      (data['user_answer'] == data['correct_answer'])
+                      ? const Color.fromARGB(255, 7, 184, 80)
+                      : const Color.fromARGB(255, 220, 20, 60),
+                  child: Text(
+                    ((data['question_index'] as int) + 1).toString(),
+                    style: const TextStyle(color: Colors.white),
                   ),
-                  const SizedBox(width: 10),
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          data['question'].toString(),
-                          style: GoogleFonts.lato(color: Colors.white),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        data['question'].toString(),
+                        style: GoogleFonts.lato(color: Colors.white),
+                      ),
+                      Text(
+                        'Your answer: ${data['user_answer']}',
+                        style: TextStyle(
+                          color: (data['user_answer'] == data['correct_answer'])
+                              ? const Color.fromARGB(255, 7, 184, 80)
+                              : const Color.fromARGB(255, 220, 20, 60),
                         ),
-                        Text(
-                          'Your answer: ${data['user_answer']}',
-                          style: TextStyle(
-                            color:
-                                (data['user_answer'] == data['correct_answer'])
-                                ? const Color.fromARGB(255, 7, 184, 80)
-                                : const Color.fromARGB(255, 220, 20, 60),
-                          ),
+                      ),
+                      Text(
+                        'Correct answer: ${data['correct_answer']}',
+                        style: const TextStyle(
+                          color: Color.fromARGB(255, 7, 184, 80),
                         ),
-                        Text(
-                          'Correct answer: ${data['correct_answer']}',
-                          style: const TextStyle(
-                            color: Color.fromARGB(255, 7, 184, 80),
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ],
-              );
-            }).toList(),
-          ),
+                ),
+              ],
+            );
+          }).toList(),
         ),
       ),
     );
